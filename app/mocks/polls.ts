bbc827cca0b4dd63a4c5481ca3b61cdf013d8ffa
Denzil Doyle <denzildoyle@gmail.com>
2025-12-07 21:14:38 -0400

    Extend poll mock

--- app/mocks/polls.ts
+++ app/mocks/polls.ts
@@ -1,3 +1,12 @@
+import { Category } from '@/constants/categories';
+
+export interface PollOption {
+  id: string;
+  text: string;
+  votes: number;
+  avatars?: string[];
+}
+
 export interface Poll {
   id: string;
   question: string;
@@ -6,61 +15,146 @@ export interface Poll {
   isOwner: boolean;
   hasResponses: boolean;
   liked: boolean;
+  category: Category;
+  options: PollOption[];
+  userVoted: boolean;
+  votingType: 'Open Voting' | 'Anonymous Voting';
 }
 
 export const mockPolls: Poll[] = [
   {
     id: '1',
-    question: 'Which Trinidad and Tobago politician would you let watch your children?',
-    votes: 4,
-    timeLeft: '23 hours left',
+    question: 'Where should we eat lunch tomorrow?',
+    votes: 45,
+    timeLeft: '3 days remaining',
     isOwner: false,
     hasResponses: true,
     liked: false,
+    category: 'Shopping',
+    userVoted: false,
+    votingType: 'Open Voting',
+    options: [
+      {
+        id: 'opt1',
+        text: 'A New Place!',
+        votes: 21,
+        avatars: ['https://i.pravatar.cc/150?img=1', 'https://i.pravatar.cc/150?img=2'],
+      },
+      {
+        id: 'opt2',
+        text: 'At Office',
+        votes: 6,
+        avatars: ['https://i.pravatar.cc/150?img=3', 'https://i.pravatar.cc/150?img=4'],
+      },
+      {
+        id: 'opt3',
+        text: 'Regular Place',
+        votes: 15,
+        avatars: ['https://i.pravatar.cc/150?img=5', 'https://i.pravatar.cc/150?img=6'],
+      },
+      {
+        id: 'opt4',
+        text: 'Any will do',
+        votes: 3,
+        avatars: ['https://i.pravatar.cc/150?img=7'],
+      },
+    ],
   },
   {
     id: '2',
-    question: 'What is the best street food in Port of Spain?',
-    votes: 12,
-    timeLeft: '18 hours left',
-    isOwner: true,
-    hasResponses: false,
+    question: 'Which Trinidad and Tobago politician would you let watch your children?',
+    votes: 28,
+    timeLeft: '2 days remaining',
+    isOwner: false,
+    hasResponses: true,
     liked: false,
+    category: 'Politics',
+    userVoted: false,
+    votingType: 'Anonymous Voting',
+    options: [
+      { id: 'opt1', text: 'Politician A', votes: 12 },
+      { id: 'opt2', text: 'Politician B', votes: 8 },
+      { id: 'opt3', text: 'Politician C', votes: 5 },
+      { id: 'opt4', text: 'None of them', votes: 3 },
+    ],
   },
   {
     id: '3',
-    question: 'Which beach in Trinidad is most overrated?',
-    votes: 8,
-    timeLeft: '15 hours left',
-    isOwner: false,
-    hasResponses: true,
-    liked: true,
+    question: 'What is the best street food in Port of Spain?',
+    votes: 42,
+    timeLeft: '4 days remaining',
+    isOwner: true,
+    hasResponses: false,
+    liked: false,
+    category: 'Shopping',
+    userVoted: false,
+    votingType: 'Open Voting',
+    options: [
+      {
+        id: 'opt1',
+        text: 'Doubles',
+        votes: 25,
+        avatars: ['https://i.pravatar.cc/150?img=8', 'https://i.pravatar.cc/150?img=9'],
+      },
+      {
+        id: 'opt2',
+        text: 'Bake and Shark',
+        votes: 10,
+        avatars: ['https://i.pravatar.cc/150?img=10'],
+      },
+      {
+        id: 'opt3',
+        text: 'Roti',
+        votes: 5,
+        avatars: ['https://i.pravatar.cc/150?img=11'],
+      },
+      { id: 'opt4', text: 'Pelau', votes: 2 },
+    ],
   },
   {
     id: '4',
-    question: 'Best time to visit Maracas Beach?',
-    votes: 20,
-    timeLeft: '22 hours left',
+    question: 'Which beach in Trinidad is most overrated?',
+    votes: 33,
+    timeLeft: '1 day remaining',
     isOwner: false,
     hasResponses: true,
-    liked: false,
+    liked: true,
+    category: 'Sport',
+    userVoted: false,
+    votingType: 'Anonymous Voting',
+    options: [
+      { id: 'opt1', text: 'Maracas Beach', votes: 18 },
+      { id: 'opt2', text: 'Las Cuevas', votes: 8 },
+      { id: 'opt3', text: 'Tyrico Bay', votes: 5 },
+      { id: 'opt4', text: 'Blanchisseuse', votes: 2 },
+    ],
   },
   {
     id: '5',
-    question: 'Which carnival band has the best costumes this year?',
-    votes: 35,
-    timeLeft: '12 hours left',
-    isOwner: true,
-    hasResponses: true,
-    liked: false,
-  },
-  {
-    id: '6',
-    question: 'What is the most underrated doubles spot?',
-    votes: 16,
-    timeLeft: '20 hours left',
+    question: 'Best time to visit Maracas Beach?',
+    votes: 50,
+    timeLeft: '5 days remaining',
     isOwner: false,
     hasResponses: true,
     liked: false,
+    category: 'Education',
+    userVoted: false,
+    votingType: 'Open Voting',
+    options: [
+      {
+        id: 'opt1',
+        text: 'Early Morning',
+        votes: 28,
+        avatars: ['https://i.pravatar.cc/150?img=12', 'https://i.pravatar.cc/150?img=13'],
+      },
+      {
+        id: 'opt2',
+        text: 'Afternoon',
+        votes: 15,
+        avatars: ['https://i.pravatar.cc/150?img=14'],
+      },
+      { id: 'opt3', text: 'Evening', votes: 5 },
+      { id: 'opt4', text: 'Night', votes: 2 },
+    ],
   },
 ];
